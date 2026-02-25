58a2c3701a31a4a6219bdcea8f4aca8186b32162
Niponpal <niponpal52@gmail.com>
2026-02-25 21:36:33 +0600

    add

---
+++ lib/controller/ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ui extends StatefulWidget {
+  const Ui({super.key});
+
+  @override
+  State<Ui> createState() => _UiState();
+}
+
+class _UiState extends State<Ui> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
