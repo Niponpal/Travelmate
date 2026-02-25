6760aeb3505bc6beb8f0b26658cf7434c57f2568
Niponpal <niponpal52@gmail.com>
2026-02-25 21:38:49 +0600

    Fix All 🚀🚀🚑

---
+++ lib/view/ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ui extends StatefulWidget {
+  const ui({super.key});
+
+  @override
+  State<ui> createState() => _uiState();
+}
+
+class _uiState extends State<ui> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
